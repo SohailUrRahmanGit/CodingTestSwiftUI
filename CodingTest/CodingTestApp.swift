c32ba3930203e533896f7412ade3a9658437e16b
SohailUrRahmanGit <sohail.u.rahman@kp.org>
2024-01-30 12:25:50 +0530

    ProjectCreation

---
+++ CodingTest/CodingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingTestApp.swift
+//  CodingTest
+//
+//  Created by Sohail Ur Rahman on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CodingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
